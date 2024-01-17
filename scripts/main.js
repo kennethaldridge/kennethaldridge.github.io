34360b15d5d2475d12c7537580b4125d3c9f7405
kennethaldridge <kenneth.r.aldridge@gmail.com>
2024-01-16 22:49:03 -0500

    changed photo

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ const myButton = document.querySelector("button")
 myButton.onclick = () => {
     const mySrc = myImage.getAttribute("src");
     if (mySrc === "images/self-portrait.png") {
-      myImage.setAttribute("src", "images/secondphoto.png");
+      myImage.setAttribute("src", "images/soccerball.png");
     } else {
       myImage.setAttribute("src", "images/self-portrait.png");
     }
