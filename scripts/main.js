721b2e08a531685257ceb358241c5fe5ced24574
kennethaldridge <kenneth.r.aldridge@gmail.com>
2024-01-16 22:37:53 -0500

    added change image

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
 document.querySelector("html").addEventListener("click", function () {
     alert("Ouch! Stop poking me!");
   });
+
+
+const myImage = document.querySelector("img");
+const myButton = document.querySelector("button")
+
+myButton.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/self-portrait.png") {
+      myImage.setAttribute("src", "images/secondphoto.png");
+    } else {
+      myImage.setAttribute("src", "images/self-portrait.png");
+    }
+  };
